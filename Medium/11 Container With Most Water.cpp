7b9a51d50cfb4caf0a3dd24b1c8e2cbe0cb31284
Subandhu <121375475+retrojokerr@users.noreply.github.com>
2023-04-22 01:11:34 +0530

    Add files via upload

---
+++ Medium/11 Container With Most Water.cpp
@@ -0,0 +1,21 @@
+//Qustion: https://leetcode.com/problems/container-with-most-water/
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int ans=0;
+        int n=height.size();
+        int i=0, j=n-1;
+        while(i<j){
+            if(height[i]>height[j]){
+                ans = max(ans, (j-i)*height[j]);
+               j--;
+            }
+            else{
+               ans = max(ans, (j-i)*height[i]);
+               i++;
+           }
+        }
+        return ans;
+    }
+};
